1baf40b6fe2ddcbfc64eb49bde5b2a9fc41591fd
Stas Samgin <stassamgin@gmail.com>
2017-07-20 01:07:22 +0300

    working with work image, js

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,10 +1,41 @@
-$(document).ready(function() {
+'use strict';
 
-    $('.menuBtn').on('click', function(event) {
-        event.preventDefault();
-        $(this).toggleClass('menuBtn--active');
-        $('.header__nav').toggleClass('header__nav--active');
-        $('.bodyWrapper').toggleClass('bodyWrapper--active');
-    });
+// EVENT MENU CLICK
 
-})
+document.querySelector('.menuBtn').onclick = function (e) {
+    e.preventDefault();
+    document.querySelector('.header__nav').classList.toggle('header__nav--active');
+}
+
+// EVENT SCROLL - BOTTOM PROGRESS BAR 
+
+var scrollProgress = document.querySelector('.scroll_progress');
+scrollProgress.style.width = 0;
+
+window.onscroll = function() {
+  var offset = window.pageYOffset || document.documentElement.scrollTop,
+  		windowHeight = document.documentElement.scrollHeight-document.documentElement.clientHeight,
+      progress = Math.floor(offset/windowHeight * 100);
+  scrollProgress.style.width = progress + '%';;
+}
+
+// EVENT MENU CLICK - SCROLL ON BLOCK
+
+document.querySelector('.header__navList').onclick = function(e) {
+    e.preventDefault();
+    var target = e.target;
+    if (target.tagName != 'A') return;
+    var href = target.getAttribute('href'),
+        elem = document.querySelector('.' + href)
+
+    window.scrollTo(0, getElementHeight(elem));
+
+    if(document.documentElement.clientWidth < 768) {
+        document.querySelector('.header__nav').classList.remove('header__nav--active');
+    }    
+}
+
+function getElementHeight (elem) {
+    var box = elem.getBoundingClientRect();
+    return box.top + pageYOffset;
+}
